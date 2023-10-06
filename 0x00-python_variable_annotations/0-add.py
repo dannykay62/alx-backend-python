857d4e62417e957ef578a042d75111cfc8df5e43
dannykay62 <danielkay62@gmail.com>
2023-10-06 02:10:05 +0100

    a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,6 +3,8 @@
     a type-annotated function add that takes a float a and a float b as
     arguments and returns their sum as a float.
 """
+
+
 def add(a: float, b: float) -> float:
     """add function"""
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,8 @@
 """a type-annotated function concat that takes a string str1 and a
     string str2 as arguments and returns a concatenated string
 """
+
+
 def concat(str1: str, str2: str) -> str:
     """Returns the result of concatenating two strings"""
     return str1 + str2
